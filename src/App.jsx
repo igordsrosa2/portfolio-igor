c0b2bb064eb75569706071395ff484b30067a47d
Igor dos Santos Rosa <igordsrosaa@gmail.com>
2025-01-21 14:37:29 -0300

    inserindo seção de habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Footer from './sections/Footer/Footer';
 import Home from './sections/Home/Home';
 import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
 
 function App() {
   return (
     <>
       <Home />
       <Projects />
+      <Skills />
       <Footer />
     </>
   );
