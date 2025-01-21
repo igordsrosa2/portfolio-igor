37a24695b92174afcf8e92f5cbfa3786a457cfa6
Igor dos Santos Rosa <igordsrosaa@gmail.com>
2025-01-21 13:22:03 -0300

    inserindo botão de sobre

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -6,15 +6,19 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/Curriculo.pdf';
 import { useTheme } from '../../common/ThemeContext';
+import { useState } from 'react';
 
 function Home() {
   const { theme, toggleTheme } = useTheme();
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const themeIcon = theme === 'light' ? sun : moon;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  const handlePopupToggle = () => setIsPopupOpen(!isPopupOpen);
 
   return (
     <section id="home" className={styles.container}>
@@ -22,7 +26,7 @@ function Home() {
         <img
           src={profilePicture}
           className={styles.home}
-          alt="Profile picture "
+          alt="Profile picture"
         />
         <img
           className={styles.colorMode}
@@ -33,7 +37,7 @@ function Home() {
       </div>
       <div className={styles.info}>
         <h1>
-          Igor dos 
+          Igor dos
           <br />
           Santos Rosa
         </h1>
@@ -47,12 +51,48 @@ function Home() {
           </a>
         </span>
         <p className={styles.description}>
-            Transformando dados em decisões estratégicas e insights valiosos..
+          Transformando dados em decisões estratégicas e insights valiosos..
         </p>
-        <a href={CV} download>
-          <button className="hover">Curriculo</button>
-        </a>
+        <div className={styles.buttons}>
+        <button className="hover" onClick={handlePopupToggle}>
+            Sobre Mim
+          </button>
+          <a href={CV} download>
+            <button className="hover">Currículo</button>
+          </a>
+        </div>
       </div>
+
+{/* Popup */}
+{isPopupOpen && (
+  <div className={styles.popupOverlay}>
+    <div className={styles.popup}>
+      <h3>Sobre Mim</h3>
+      <div className={styles.popupContent}>
+        <div className={styles.popupSection}>
+          <h4>Nome</h4>
+          <p>Igor dos Santos Rosa</p>
+        </div>
+        <div className={styles.popupSection}>
+          <h4>Idade</h4>
+          <p>25 anos</p>
+        </div>
+        <div className={styles.popupSection}>
+          <h4>Formação</h4>
+          <p>Curso superior em tecnologia em análise e desenvolvimento de Sistemas (JAN - 2023 / DEZ 2024) - Senai São Caetano</p><br />
+          <p>Curso técnico em eletroeletrônica (JUN - 2017 / JUN 2019) - Senai Santo André</p>
+        </div>
+        <div className={styles.popupSection}>
+          <h4>Experiência</h4>
+          <p>Embora ainda não possua experiência profissional na área de TI, estou determinado a desenvolver habilidades práticas e adquirir conhecimento através de desafios e oportunidades no setor. Busco crescimento profissional e estou comprometido em contribuir ativamente para o sucesso de projetos e equipes.</p>
+        </div>
+      </div>
+      <button onClick={handlePopupToggle} className={styles.closePopup}>
+        Fechar
+      </button>
+    </div>
+  </div>
+)}
     </section>
   );
 }
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
               <h3>{project.name}</h3>
               <p>{project.description}</p>
               <a href={project.link} target="_blank" rel="noopener noreferrer">
-                Ver mais
+                Acessar o repositório
               </a>
             </div>
           </div>
