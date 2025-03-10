588aad77de233de36780456097fc5cce5155283a
Igor dos Santos Rosa <igordsrosaa@gmail.com>
2025-03-10 15:18:59 -0300

    atualizando logos dos projetos

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import styles from './ProjectsStyles.module.css';
+import React from "react";
+import styles from "./ProjectsStyles.module.css";
 
 // Importe as imagens dos projetos
-import ucl from '../../assets/logoucl.svg';
-import nike from '../../assets/nike.svg';
-import logoTEF from '../../assets/logoTEF.png'
+import logoUefa from "../../assets/logouefa.png";
+import nike from "../../assets/logonike.png";
+import logoTEF from "../../assets/logoTEF.png";
 
 const projects = [
   {
     id: 1,
     name: "UEFA CHAMPIONS LEAGUE",
-    image: ucl,
-    description: "Análise e exploração do conjunto de dados que contém as principais estatísticas dos jogadores da temporada 2021-22 da Liga dos Campeões da UEFA .",
+    image: logoUefa,
+    description:
+      "Análise e exploração do conjunto de dados que contém as principais estatísticas dos jogadores da temporada 2021-22 da Liga dos Campeões da UEFA .",
     link: "https://github.com/igordsrosa2/ucl22-analysis",
-    link2: "https://app.powerbi.com/view?r=eyJrIjoiYzM0MmViNzItMzU5NC00OWMwLTk1MjQtOTM3NDhmYTRlZTZmIiwidCI6IjdjZDc0M2Y4LTA5MTgtNGU2OC04NGU5LTlkY2Q4MmU0ODBhZiJ9"
+    link2:
+      "https://app.powerbi.com/view?r=eyJrIjoiYzM0MmViNzItMzU5NC00OWMwLTk1MjQtOTM3NDhmYTRlZTZmIiwidCI6IjdjZDc0M2Y4LTA5MTgtNGU2OC04NGU5LTlkY2Q4MmU0ODBhZiJ9",
   },
   {
     id: 2,
     name: "Nike Global Sales",
     image: nike,
-    link: 'https://github.com/igordsrosa2/nike-analysis',
-    link2: 'https://app.powerbi.com/view?r=eyJrIjoiNzkyYmY1NjQtMmYyOC00YWZkLTkyYzAtODI4ZmYxYzc4YzNkIiwidCI6IjdjZDc0M2Y4LTA5MTgtNGU2OC04NGU5LTlkY2Q4MmU0ODBhZiJ9',
-    description: "Análise dos dados de vendas da Nike no ano de 2024, com o objetivo de entender o desempenho da marca em diferentes mercados e identificar tendências de consumo.",
+    link: "https://github.com/igordsrosa2/nike-analysis",
+    link2:
+      "https://app.powerbi.com/view?r=eyJrIjoiNzkyYmY1NjQtMmYyOC00YWZkLTkyYzAtODI4ZmYxYzc4YzNkIiwidCI6IjdjZDc0M2Y4LTA5MTgtNGU2OC04NGU5LTlkY2Q4MmU0ODBhZiJ9",
+    description:
+      "Análise dos dados de vendas da Nike no ano de 2024, com o objetivo de entender o desempenho da marca em diferentes mercados e identificar tendências de consumo.",
   },
-    {
+  {
     id: 2,
     name: "Sistema de Jogos TEF",
     image: logoTEF,
-    link: 'https://github.com/igordsrosa2/JogosTEF',
-    description: "sistema que permite controlar (registrar, editar e excluir) as partidas realizadas pelo time TEF em 2025",
-  }
+    link: "https://github.com/igordsrosa2/JogosTEF",
+    description:
+      "sistema que permite controlar (registrar, editar e excluir) as partidas realizadas pelo time TEF em 2025",
+  },
 ];
 
 const Projects = () => {
@@ -39,7 +44,11 @@ const Projects = () => {
       <div className={styles.projectList}>
         {projects.map((project) => (
           <div key={project.id} className={styles.projectCard}>
-            <img src={project.image} alt={project.name} className={styles.projectImage} />
+            <img
+              src={project.image}
+              alt={project.name}
+              className={styles.projectImage}
+            />
             <div className={styles.projectInfo}>
               <h3>{project.name}</h3>
               <p>{project.description}</p>
@@ -47,7 +56,8 @@ const Projects = () => {
               <a href={project.link} target="_blank" rel="noopener noreferrer">
                 Acessar o repositório
               </a>
-              <br /><br />
+              <br />
+              <br />
             </div>
           </div>
         ))}
